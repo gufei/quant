3dd2f05a398e304a150dbda91c861693fb6bb7fa
rowan.wu <>
2023-06-07 11:10:06 +0800

    fix

--- run.py
+++ run.py
@@ -11,9 +11,11 @@ import models.strategy.strategy_my01 as my_strategy
 
 import akshare as ak
 
+import qstock as qs
+
 # 初期资产
 cash = 200000
-symbol = "000906"
+symbol = "zz800"
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
@@ -28,10 +30,12 @@ if __name__ == '__main__':
     # 每批最少100股
     cerebro.addsizer(bt.sizers.FixedSize, stake=100)
 
-    index_stock_cons_csindex_df = ak.index_stock_cons_csindex(symbol=symbol)
+    # index_stock_cons_csindex_df = ak.index_stock_cons_csindex(symbol=symbol)
+
+    index_stock_cons_csindex_df = qs.index_member(symbol)
 
     for _, stock in index_stock_cons_csindex_df.iterrows():
-        file = "./data/day/" + stock['成分券代码'] + ".csv"
+        file = "./data/day/" + stock['股票代码'] + ".csv"
         data = bt.feeds.GenericCSVData(
             dataname=file,
             fromdate=datetime.datetime(2023, 1, 1),
