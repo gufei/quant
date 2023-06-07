3a5178f3e49a0d2fcecc0b0c0653d3324e553a11
rowan.wu <>
2023-06-07 18:53:03 +0800

    直接发送内容

--- cron/news.py
+++ cron/news.py
@@ -6,11 +6,12 @@ sys.path.append(os.path.dirname(sys.path[0]))
 import datetime
 import qstock as qs
 import utils.notify as notify
+
 # 10分钟内新闻
 release_time = (datetime.datetime.now() + datetime.timedelta(minutes=-10)).time()
 
 df = qs.news_data()
 
 for _, new in df[df.发布时间 > release_time].iterrows():
-    msg = new['标题'] + "\n" + new['内容']
+    msg = new['内容']
     notify.send_msg_by_redis(msg)
