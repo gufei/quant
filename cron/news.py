0a80dc197c45e7b6a6e6a22d017567d15f2b964b
rowan.wu <>
2023-06-08 10:44:08 +0800

    修改获取条件

--- cron/news.py
+++ cron/news.py
@@ -13,9 +13,7 @@ release_datetime = (datetime.datetime.now() + datetime.timedelta(minutes=-10))
 
 df = qs.news_data()
 
-day_df = df[df.发布日期 == release_datetime.date()]
-
-for _, new in day_df[day_df.发布时间 > release_datetime.time()].iterrows():
+for _, new in df[(df.发布日期 == release_datetime.date()) & (df.发布时间 > release_datetime.time())].iterrows():
     msg = new['内容']
     notify.send_msg_by_redis(msg)
     time.sleep(5)
