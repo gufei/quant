232e50bbfe63388c08aa1b77baf9e85cbe3555ec
rowan.wu <>
2023-07-06 13:21:59 +0800

    正则替换

--- cron/news.py
+++ cron/news.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(sys.path[0]))
 import datetime
 import qstock as qs
 import utils.notify as notify
+import re
 
 # 10分钟内新闻
 release_datetime = (datetime.datetime.now() + datetime.timedelta(minutes=-10))
@@ -15,5 +16,7 @@ df = qs.news_data()
 
 for _, new in df[(df.发布日期 == release_datetime.date()) & (df.发布时间 > release_datetime.time())].iterrows():
     msg = new['内容']
+    pattern = r"财联社\d+月\d+日电，"
+    msg = re.sub(pattern, "", msg)
     notify.send_msg_by_redis("news", msg)
     time.sleep(5)
